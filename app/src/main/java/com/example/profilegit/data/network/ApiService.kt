08ac90e8cc57ae0f8704de225fd87484c27aee20
Vlad Lubin <lubinvlad2@gmail.com>
2023-12-13 01:25:09 +0200

    use cases

--- app/src/main/java/com/example/profilegit/data/network/ApiService.kt
+++ app/src/main/java/com/example/profilegit/data/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.profilegit.data.network
 
+import com.example.profilegit.data.dto.DetailsDto
 import com.example.profilegit.data.dto.UserDto
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,5 +9,5 @@ interface ApiService {
     @GET("users")
     suspend fun getUsers(): List<UserDto>
     @GET("/users/{login}")
-    suspend fun getUserDetails(@Path("login") login:String)
+    suspend fun getUserDetails(@Path("login") login:String): DetailsDto
 }
--- app/src/main/java/com/example/profilegit/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/profilegit/data/repository/RepositoryImpl.kt
@@ -1,9 +1,20 @@
 package com.example.profilegit.data.repository
 
+import com.example.profilegit.data.dto.DetailsDto
+import com.example.profilegit.data.dto.UserDto
+import com.example.profilegit.data.network.ApiService
+import com.example.profilegit.domain.repository.Repository
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
+    private val api: ApiService
+) : Repository {
+    override suspend fun getUsers(): List<UserDto> {
+        return api.getUsers()
+    }
 
-){
+    override suspend fun getUserDetails(login: String): DetailsDto {
+        return api.getUserDetails(login)
+    }
 
 }
